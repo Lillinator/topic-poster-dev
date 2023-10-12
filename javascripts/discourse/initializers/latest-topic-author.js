08c17b814aab50f2cf2da2aa8454ebddf0fc8e15
Lilly <128197764+Lillinator@users.noreply.github.com>
2023-10-12 16:50:32 -0700

    Update latest-topic-author.js

--- javascripts/discourse/initializers/latest-topic-author.js
+++ javascripts/discourse/initializers/latest-topic-author.js
@@ -1,5 +1,5 @@
 import { apiInitializer } from "discourse/lib/api";
-import LatestTopicAuthor from "../components/latest-topic-list-author";
+import LatestTopicAuthor from "../components/latest-topic-author";
 
 export default apiInitializer("1.14.0", (api) => {
   api.renderInOutlet("above-latest-topic-list-item", LatestTopicAuthor);
