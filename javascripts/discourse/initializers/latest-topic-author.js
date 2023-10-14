e9a91043b5048d64f405ebe7c9223dc938de44ac
Lilly <128197764+Lillinator@users.noreply.github.com>
2023-10-13 17:45:24 -0700

    Delete javascripts/discourse/initializers/latest-topic-author.js

--- javascripts/discourse/initializers/latest-topic-author.js
+++
@@ -1,6 +0,0 @@
-import { apiInitializer } from "discourse/lib/api";
-import LatestTopicAuthor from "../components/latest-topic-author";
-
-export default apiInitializer("1.14.0", (api) => {
-  api.renderInOutlet("above-latest-topic-list-item", LatestTopicAuthor);
-});
